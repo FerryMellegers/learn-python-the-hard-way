954b729ee0da584187328068e4f356755f9613b6
Buunshin <ferrymellegers@hotmail.com>
2017-09-03 18:19:28 +0200

    Finisher exercise 3

---
+++ ex3.py
@@ -0,0 +1,27 @@
+# The Print function prints out the characters between the quotes
+print "I will now count my chickens:"
+# Everything behind the comma is separated from what was written before,
+# Which means you can plot an oquasion after you printed something
+print "Hens", 25.0 + 30 / 6
+print "Roosters", 100.0 - 25 * 3 % 4
+# A simple print function
+print "Now I will count the eggs:"
+# The plus sign adds
+# The minus sign subtracts
+# The percent sign plots the number thats left after subtracting the second number after first number
+print 3.0 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+# Prints an oquasion using the less than sign
+print "Is it true that 3.0 + 2 < 5 - 7?"
+# Plots the oquasion using less than, which checks if the first part is less than the second part
+print 3.0 + 2 < 5 - 7
+# Prints two simple oquasions
+print "What is 3 + 2?", 3.0 + 2
+print "What is 5 - 7?", 5.0 - 7
+# Prints a comment
+print "Oh, that's why it's False."
+# Prints a comment
+print "How about some more."
+# Plots some oquasion using the bigger than, bigger than or equal and the smaller than or equal sign
+print "Is is greater?", 5.0 > -2
+print "Is it greater or equal?", 5.0 >= -2
+print "Is it less or equal?", 5.0 <= -2
